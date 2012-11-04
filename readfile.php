43472462473680189f6848b85469077c4a056e41
Andriy Noble <andriymn@gmail.com>
2012-11-03 20:48:13 -0400

    adding

--- readfile.php
+++ readfile.php
@@ -3,7 +3,7 @@ function readfile() {
 	$handle = @fopen("inputfile.txt", "r");
 	if ($handle) {
 	    while (($buffer = fgets($handle, 4096)) !== false) {
-	        echo $buffer;
+	        echo $buffer . "\n";
 	    }
 	    if (!feof($handle)) {
 	        echo "Error: unexpected fgets() fail\n";
